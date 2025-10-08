97c508fe32dae1ab29aaa711e7c6b7abe78f7794
goodlckkk <smaringonz@gmail.com>
2025-10-08 00:43:27 -0300

    Config BDO v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Módulo para cargar variables de entorno (.env)
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que las variables estén disponibles en toda la app
+    }),
+
+    // 2. Módulo de TypeORM para la conexión a la base de datos
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [], // Dejaremos esto vacío por ahora
+      synchronize: true, // En desarrollo, crea las tablas automáticamente. ¡No usar en producción!
+      
+    }),   
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
