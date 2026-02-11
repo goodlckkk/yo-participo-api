848b04601be52c6c34e9978d7340b01b523f7e5b
goodlckkk <smaringonz@gmail.com>
2026-02-11 15:16:28 -0300

    add new config

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,8 @@ import { HeroSlide } from './hero-slides/entities/hero-slide.entity';
 import { SuccessStoriesModule } from './success-stories/success-stories.module';
 import { SuccessStory } from './success-stories/entities/success-story.entity';
 import { EmailsModule } from './emails/emails.module';
+import { AuditLogsModule } from './audit-logs/audit-logs.module';
+import { AuditLog } from './audit-logs/entities/audit-log.entity';
 
 @Module({
   imports: [
@@ -53,7 +55,7 @@ import { EmailsModule } from './emails/emails.module';
         username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_DATABASE'),
-        entities: [Sponsor, User, Trial, PatientIntake, ResearchSite, Cie10Code, HeroSlide, SuccessStory],
+        entities: [Sponsor, User, Trial, PatientIntake, ResearchSite, Cie10Code, HeroSlide, SuccessStory, AuditLog],
         synchronize: false, // SIEMPRE false - usamos migraciones
         ssl: {
           rejectUnauthorized: false,
@@ -72,6 +74,7 @@ import { EmailsModule } from './emails/emails.module';
     HeroSlidesModule,
     SuccessStoriesModule,
     EmailsModule,
+    AuditLogsModule,
   ],
   controllers: [AppController],
   providers: [
